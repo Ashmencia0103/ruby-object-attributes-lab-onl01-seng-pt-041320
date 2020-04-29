2d852d75a6774cfc96c6eab753b32a917a51cbeb
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-29 20:00:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,23 @@
 class Dog
-  # def initialize(name)
-  # @name = name
-  # end
-  # def name
-  #   @name
-  # end
-  # def name=(new_name)
-  #   @name = new_name
-  # end
+def name=(dogs_name)
+  @this_dogs_name = dogs_name
 end
+def name
+  @this_dogs_name
+end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+
+
+# def initialize(name)
+# @name = name
+# end
+# def name
+#   @name
+# end
+# def name=(new_name)
+#   @name = new_name
+# end
